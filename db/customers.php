c1d0edf8a2d661c5cb5f1ec0540eebc59d56a63d
soniaarnaudova <soniaarnaudova@yahoo.com>
2019-10-19 13:57:28 +0300

    Update customers.php

--- db/customers.php
+++ db/customers.php
@@ -17,39 +17,39 @@ include("dbConfig.php");
             <th>Phone</th>
             <th>Email</th>
             <th>Newsletter</th>
-       </tr>
+          </tr>
          </thead>');
-    $query = "SELECT * FROM users;";
-    
-     $results = mysqli_query($db, $query);
-    if ($results) {
-    while ($item = $results->fetch_assoc()) {
-        $id = $item["id"];
-        $name = $item["name"];
-        $address = $item["address"];
-        $phone = $item["phone"];
-        $email = $item["email"];
-        $newsletter = $item["newsletter"];
-        
-        
-       
-        echo('<tbody>
-           <tr>
-                <td><input type="text" readonly name="c_id" size="3" value="'.$id.'"/></td>
-                <td>'.$name.'</td>
-                <td>'.$address.'</td>
-                <td>'.$phone.'</td>
-                <td>'.$email.'</td>');
-                if($newsletter==1){
-                   echo('<td>Yes</td>');
-                }else{
-                   echo('<td>No</td>');
-                 }
-           echo('</tr>
-         </tbody>');
-        
-    }
-  }
-?>
+          $query = "SELECT * FROM users;";
+
+           $results = mysqli_query($db, $query);
+          if ($results) {
+          while ($item = $results->fetch_assoc()) {
+              $id = $item["id"];
+              $name = $item["name"];
+              $address = $item["address"];
+              $phone = $item["phone"];
+              $email = $item["email"];
+              $newsletter = $item["newsletter"];
+
+
+
+              echo('<tbody>
+                 <tr>
+                      <td><input type="text" readonly name="c_id" size="3" value="'.$id.'"/></td>
+                      <td>'.$name.'</td>
+                      <td>'.$address.'</td>
+                      <td>'.$phone.'</td>
+                      <td>'.$email.'</td>');
+                      if($newsletter==1){
+                         echo('<td>Yes</td>');
+                      }else{
+                         echo('<td>No</td>');
+                       }
+                 echo('</tr>
+               </tbody>');
+
+          }
+        }
+      ?>
     </body>
 </html>
